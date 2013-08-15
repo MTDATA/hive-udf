845f5f316fcae57eba14b99ffff302f8b9b90708
jhaoniu <jhaoniu@163.com>
2013-08-15 08:51:35 +0800

    URLDecode 编码方式可以配置

--- src/main/java/com/sankuai/meituan/hive/udf/URLDecode.java
+++ src/main/java/com/sankuai/meituan/hive/udf/URLDecode.java
@@ -20,9 +20,19 @@ public class URLDecode extends UDF {
     public URLDecode() {
     }
 
+    /**
+     * 默认编码utf-8
+     * 
+     * @param key
+     * @return
+     */
     public static String evaluate(final String key) {
+        return evaluate(key, "utf-8");
+    }
+
+    public static String evaluate(final String key, final String enc) {
         try {
-            return URLDecoder.decode(key, "utf-8");
+            return URLDecoder.decode(key, enc);
         } catch (UnsupportedEncodingException e) {
             return "";
         }
