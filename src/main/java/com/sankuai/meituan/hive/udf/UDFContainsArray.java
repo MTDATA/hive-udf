06fca42f2e4b7a9241fb8dc92d4e4e8dd0be6668
jason-wjian <jason.wjian@gmail.com>
2013-11-04 17:55:43 +0800

    Create UDFContainsArray.java
    A udf to count how many str2s are contained in str1, return the number, for example evaluate("abcdabc","abc") will return 2.
    
    This udf has been tested in an offline hive environment, also add a test file TestUDFContainsArray.java to /hive-udf-master/test/src/main/java/com/sankuai/meituan/hive/udf/

---
+++ src/main/java/com/sankuai/meituan/hive/udf/UDFContainsArray.java
@@ -0,0 +1,50 @@
+package com.sankuai.meituan.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.junit.Assert;
+
+/**
+ * udf contains_arr
+ * 
+ * @author wangjian
+ * @version 1.0
+ * @created 2013-10-17
+ *
+ */
+@Description(name = "contains_arr", 
+value = "_FUNC_(a1,a2) - Return how many array a2s are contained in array a1")
+public class UDFContainsArray extends UDF {
+	
+	public UDFContainsArray() {
+		
+	}
+	
+	public int evaluate(String str1, String str2) {
+		if (str1 == null || str2 == null) {
+			return 0;
+		}
+		char[] a1 = str1.toCharArray();
+		char[] a2 = str2.toCharArray();
+		if (a1.length == 0 || a1.length < a2.length) {
+			return 0;
+		}
+		int sum = 0;
+		for (int i = 0; i < a1.length - a2.length + 1; i++) {
+			if (a1[i] == a2[0]) {
+				boolean flag = true;
+				for (int j = 1; j < a2.length; j++) {
+					if (a1[i+j] != a2[j]) {
+						flag = false;
+						break;
+					}
+				}
+				if (flag) {
+					sum++;
+				}
+			}
+		}
+		return sum;
+	}
+
+}
